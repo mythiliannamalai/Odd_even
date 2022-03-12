aed631973dc0f388be7f71acd38dbd27a1085c24
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-12 20:30:52 +0530

    Odd_even

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Odd_even
+{
+    class Program
+    {
+        public void Calculation()
+        {
+         int GET_NUM;
+            Console.WriteLine("Enter the value : ");
+            GET_NUM =int.Parse(Console.ReadLine());
+            if(GET_NUM %2 == 0)
+            {
+                Console.WriteLine("{0} is Even number....", GET_NUM);
+            }
+          else
+            {
+                Console.WriteLine("{0} is Odd number....",GET_NUM);
+            }       
+        }
+    }
+    class Result
+    {
+        public static void Main(string[]args)
+        {
+            Program pro=new Program();
+            pro.Calculation();
+        }
+    }
+}
